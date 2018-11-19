eb004e2623847fc2ab904520fcd410936ea62674
shark <shark@163.com>
2018-11-19 14:48:03 +0800

    尝试

--- spmakete/apps/user/models.py
+++ spmakete/apps/user/models.py
@@ -1,13 +1,54 @@
 from django.db import models
 
 # Create your models here.
-
 class Users(models.Model):
-    username = models.CharField(max_length=100,verbose_name='用户名')
-    password = models.DateField(verbose_name='密码')
+    # 性别选项
+    sex_choices = (
+        (1, '男'),
+        (2, "女"),
+        (3, "保密"),
+    )
+    nickname = models.CharField(verbose_name='昵称',
+                                max_length=20,
+                                null=True,
+                                blank=True,
+                                )
+    username = models.CharField(verbose_name='手机号码',
+                             unique=True,
+                             max_length=12)
+    password = models.CharField(verbose_name='密码',
+                                max_length=64,
+                                )
+    gender = models.SmallIntegerField(verbose_name='性别',
+                                      default=3,
+                                      choices=sex_choices,
+                                      )
+    head = models.ImageField(verbose_name='用户头像',
+                             upload_to='head/%Y/%m',
+                             default='default/infortx.png',
+                             )
+
+    birthday = models.DateField(verbose_name='出生日期',
+                                null=True,
+                                blank=True,
+                                )
+    school_name = models.CharField(verbose_name='学校名称',
+                                   max_length=50,
+                                   null=True, blank=True,
+                                   )
+    address = models.CharField(verbose_name='学校详细地址',
+                               max_length=100,
+                               null=True,
+                               blank=True,
+                               )
+    hometown = models.CharField(verbose_name='老家',
+                                max_length=100,
+                                null=True, blank=True,
+                                )
 
     class Meta:
-        db_table = 'users'
+        verbose_name = '用户管理'
+        verbose_name_plural = verbose_name
 
     def __str__(self):
         return self.username
--- spmakete/apps/user/views.py
+++ spmakete/apps/user/views.py
@@ -1,14 +1,37 @@
-from django.shortcuts import render
+import hashlib
+
+from django.shortcuts import render, redirect
 
 
 # Create your views here.
+from apps.user.models import Users
+
 
 def index(request):
     return render(request, 'user/member.html')
 
 
 def login(request):
-    return render(request, 'user/login.html')
+    #接受
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+    #处理
+        try:
+            user = Users.objects.get(username=username)
+        except Users.MultipleObjectsReturned:
+            return redirect("user:用户登录")
+        except Users.DoesNotExist:
+            return redirect("user:用户登录")
+        h = hashlib.md5(password.encode("utf-8"))
+        password = h.hexdigest()
+        if password != user.password:
+            return redirect("user:用户登录")
+        request.session['ID'] = user.pk
+        request.session['username'] = user.username
+        return redirect("user:用户首页")
+    else:
+        return render(request, 'user/login.html')
 
 
 def register(request):
